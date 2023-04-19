e5b33802d9d6fc86fd07b631ae66baaaa6202e72
Maricelahuaman <maricelahuamanpareja38@gmail.com>
2023-04-19 10:00:08 -0500

    mi primer plantilla

---
+++ card.php
@@ -0,0 +1,84 @@
+<div class="container my=3">
+
+<h1>Mi página web Zamira</h1>
+
+<div class="row">
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+  <div class="card">
+    <img src="img/ropa.jpg" alt="ropita 1" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Ropa de mujer</h5>
+      <p class="card-text">Combinaciones deslumbrantes
+Esta blusa versátil se adapta fácilmente a cualquier tipo de outfit. Combínala con unos pantalones ajustados y unos tacones altos para una velada elegante, o con unos vaqueros y unas zapatillas cómodas para un look más casual y desenfadado. No importa cómo la uses, esta blusa se convertirá en una de tus favoritas, ofreciéndote infinitas posibilidades para expresar tu estilo único y personal.</p>
+      <a href="#" class="btn btn-primary">Comprar ahora</a>
+      <a href="#" class="btn btn-primary">Modos de pago</a>
+    </div>
+  </div>
+</div>
+
+</div>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>&copy; Zamira Panebra- Mi colección de ropas</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
